52945a2b092c087e5ead298846c436c2793301e6
sassembla <toru.inoue@kissaki.tv>
2013-01-13 20:58:54 +0900

    add codes

---
+++ src/main/scala/com/kissaki/KSScMessenger/KSScMessenger.scala
@@ -0,0 +1,17 @@
+// import akka.actors._
+
+
+
+
+// class  KSScMessenger () extends Actor {
+//   println("ここに来てる")
+// 	val system = ActorSystem("Hoge")
+//   //val hogeActor = system.actorOf(Props[KSScMessenger])
+
+
+//   // hogeActor ! 100
+//   // hogeActor ! "taro"
+
+//   system.shutdown()
+
+// }
---
+++ src/main/scala/com/kissaki/SampleAppMain.scala
@@ -0,0 +1,7 @@
+package com.kissaki
+
+object SampleAppMain {
+	def main(args: Array[String]) = {
+		println("hereComes")
+	}
+}

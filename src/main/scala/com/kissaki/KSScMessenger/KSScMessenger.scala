7f2817ab7178e11857781564bb2830ac9d2f9112
sassembla <toru.inoue@kissaki.tv>
2013-01-13 21:01:23 +0900

    add akka(not yet )

--- src/main/scala/com/kissaki/KSScMessenger/KSScMessenger.scala
+++ src/main/scala/com/kissaki/KSScMessenger/KSScMessenger.scala
@@ -1,17 +1,17 @@
-// import akka.actors._
+import akka.actors._
 
 
 
 
-// class  KSScMessenger () extends Actor {
-//   println("ここに来てる")
-// 	val system = ActorSystem("Hoge")
-//   //val hogeActor = system.actorOf(Props[KSScMessenger])
+class  KSScMessenger () extends Actor {
+  println("ここに来てる")
+	val system = ActorSystem("Hoge")
+  //val hogeActor = system.actorOf(Props[KSScMessenger])
 
 
-//   // hogeActor ! 100
-//   // hogeActor ! "taro"
+  // hogeActor ! 100
+  // hogeActor ! "taro"
 
-//   system.shutdown()
+  system.shutdown()
 
-// }
+}

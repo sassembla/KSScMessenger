be01ce641ce53427a87a46be0e636241b949a293
sassembla <toru.inoue@kissaki.tv>
2013-01-13 20:58:48 +0900

    moved

---
+++ src/test/scala/com/kissaki/KSScMessengerTests/KSScMessengerTests.scala
@@ -0,0 +1,29 @@
+package com.kissaki.ksscmessenger.KSScMessengerTests
+
+import org.specs2.mutable._
+import org.specs2.runner.JUnitRunner
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class KSScMessengerTests extends Specification {
+
+  /*
+	 * テストを書いてく。
+	 */
+  "test" should {
+    "something" in {
+      val s = "something"
+
+      //文字列がマッチ
+      s must be_==("something")
+    }
+  }
+
+  /*
+  centralがなくせる筈なので、そのへんからやってみるか。
+
+  */
+
+
+
+}
